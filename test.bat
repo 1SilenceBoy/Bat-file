69300613d9e615e1a6435c0f7419f0ad96539a35
1SilenceBoy <103421656+1SilenceBoy@users.noreply.github.com>
2024-07-26 17:02:05 +0800

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,8 +1,34 @@
 @echo off
 set JSF="%TMP%\EC_URI.JS"
 echo=WScript.echo(encodeURIComponent(WScript.Arguments(0)));>%JSF%
-set/pSTR=�������ַ���
+set/pSTR=ÇëÊäÈë×Ö·û´®
 cscript //nologo %JSF% "%STR%"
 pause
 
 http://bbs.bathome.net/thread-6084-1-1.html
+
+
+
+
+@echo off  
+setlocal enabledelayedexpansion  
+  
+:: 设置 JScript 文件的路径  
+set JSF="EC_URI.JS"  
+  
+:: 创建 JScript 文件  
+echo WScript.echo(encodeURIComponent(WScript.Arguments(0)));>%JSF%  
+  
+:: 提示用户输入字符串  
+set /p STR=请输入字符串:   
+  
+:: 使用 for /f 循环捕获 cscript 的输出  
+for /f "delims=" %%i in ('cscript //nologo "%JSF%" "!STR!"') do set ENCODED=%%i  
+  
+:: 显示编码后的字符串  
+echo 编码后的字符串是: !ENCODED!  
+  
+:: 清理临时文件  
+del "%JSF%"  
+  
+pause
